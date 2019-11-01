90f5a8c7b08da5558d570f47624dd8f7eac2269f
odan <odan3240@gmail.com>
2019-11-02 03:45:19 +0900

    できた

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
+import { join } from "path";
+
 import { Module } from "@nestjs/common";
+import { ServeStaticModule } from "@nestjs/serve-static";
+
 import { AppController } from "./app.controller";
 import { AuthModule } from "./auth/auth.module";
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, "..", "..", "public")
+    })
+  ],
   controllers: [AppController],
   providers: []
 })
